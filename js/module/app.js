395751710f65587e82714f0af1be4999bd6721a2
Enrico Feraldo Kalengkongang <nrcfrld@gmail.com>
2021-09-08 19:14:30 +0700

    Pertemuan 2-2

--- js/module/app.js
+++ js/module/app.js
@@ -1,1 +1,15 @@
 let app = angular.module("FirstApp", []);
+app.filter("rupiah", function () {
+	return function (angka) {
+		var rev = parseInt(angka, 10).toString().split("").reverse().join("");
+		console.log(rev);
+		var rev2 = "";
+		for (var i = 0; i < rev.length; i++) {
+			rev2 += rev[i];
+			if ((i + 1) % 3 === 0 && i !== rev.length - 1) {
+				rev2 += ".";
+			}
+		}
+		return "Rp. " + rev2.split("").reverse().join("") + ",00";
+	};
+});
