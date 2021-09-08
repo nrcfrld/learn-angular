922f13fa931da34581fb2f5c4261314a0b3101cf
Enrico Feraldo Kalengkongang <nrcfrld@gmail.com>
2021-09-08 20:18:08 +0700

    pertemuan 3

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,11 +2,36 @@ app.controller("MainController", [
 	"$scope",
 	function ($scope) {
 		$scope.title = "Halo saya Rico";
-		$scope.book = {
-			title: "Belajar AngularJs Bersama Agung Setiawan",
-			author: "Agung Setiawan",
-			price: 80000,
-			pubdate: new Date("2015", "09", "01"),
+		$scope.books = [
+			{
+				title: "Belajar AngularJs Bersama Agung Setiawan",
+				author: "Agung Setiawan",
+				price: 80000,
+				pubdate: new Date("2015", "09", "09"),
+				likes: 0,
+			},
+			{
+				title: "Belajar Ruby",
+				author: "Penulis Satu",
+				price: 65000,
+				pubdate: new Date("2015", "10", "09"),
+				likes: 0,
+			},
+			{
+				title: "Belajar Ruby on Rails",
+				author: "Penulis Dua",
+				price: 90000,
+				pubdate: new Date("2015", "11", "09"),
+				likes: 0,
+			},
+		];
+
+		$scope.logToConsole = function (index) {
+			var book = $scope.books[index];
+			console.log(book);
+		};
+		$scope.likes = function (index) {
+			$scope.books[index].likes += 1;
 		};
 	},
 ]);
--- js/module/app.js
+++ js/module/app.js
@@ -2,7 +2,6 @@ let app = angular.module("FirstApp", []);
 app.filter("rupiah", function () {
 	return function (angka) {
 		var rev = parseInt(angka, 10).toString().split("").reverse().join("");
-		console.log(rev);
 		var rev2 = "";
 		for (var i = 0; i < rev.length; i++) {
 			rev2 += rev[i];
